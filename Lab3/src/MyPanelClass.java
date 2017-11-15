ce9c7d517fe7ce97a2d7f3eb27a3a0d21bfa9e27
Joangel <Joangel@10.24.29.114>
2017-11-15 17:42:17 -0400

    Learning use Polygon
    Create an arrow and star with polygons

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,13 +1,16 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
 
 public class MyPanelClass extends JPanel 
 {
 	private static final long serialVersionUID = 7057541440811488699L;
 
-	public void paintComponent(Graphics g)	{
+	public void paintComponent(Graphics g)	
+	{
 		super.paintComponent(g);
 
 		// Compute interior coordinates
@@ -35,8 +38,35 @@ public class MyPanelClass extends JPanel
 //        g.setColor(Color.WHITE);
 //        g.drawLine(x1, y1, x2, y2);
 		  
-		  //Draw a Oval
-		  g.setColor(Color.GREEN);
-          g.fillOval(width/2-25,height/2-25, 55,55);
+//		  //Draw a Oval
+//		  g.setColor(Color.GREEN);
+//        g.fillOval(width/2-25,height/2-25, 55,55);
+		
+//		//Draw a Polygon
+//		  Polygon p = new Polygon();
+//        p.addPoint(x1 + 5, y1 + 25);
+//        p.addPoint(x1 + 20, y1 + 10);
+//        p.addPoint(x1 + 35, y1 + 25);
+//        p.addPoint(x1 + 25, y1 + 25);
+//        p.addPoint(x1 + 25, y1 + 45);
+//        p.addPoint(x1 + 15, y1 + 45);
+//        p.addPoint(x1 + 15, y1 + 25);
+//        g.setColor(Color.YELLOW);
+//        g.fillPolygon(p);
+        
+        //Draw a Polygon
+        Polygon p2 = new Polygon();
+        p2.addPoint(x1 + 25, y1 + 73);
+        p2.addPoint(x1 + 41, y1 + 73);
+        p2.addPoint(x1 + 47, y1 + 58);
+        p2.addPoint(x1 + 53, y1 + 73);
+        p2.addPoint(x1 + 69, y1 + 73);
+        p2.addPoint(x1 + 56, y1 + 83);
+        p2.addPoint(x1 + 61, y1 + 98);
+        p2.addPoint(x1 + 47, y1 + 88);
+        p2.addPoint(x1 + 34, y1 + 98);
+        p2.addPoint(x1 + 38, y1 + 83);
+        g.setColor(Color.WHITE);
+        g.drawPolygon(p2);
 	}
 }
