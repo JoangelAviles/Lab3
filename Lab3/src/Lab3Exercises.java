2bd73cb3b401e05134a0f78fe541530ffce4d46a
Joangel <Joangel@10.24.29.114>
2017-11-15 16:52:00 -0400

    Lab3
    In this version myFrame.setLocation(200,200); was changed to
    myFrame.setLocationRelativeTo(null); this change make that the
    application windows show up in the middle of the display. Also
    uncommented the setDefaultCloseOperation and changed the title.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,11 +5,8 @@ public class Lab3Exercises
     public static void main(String[] args) 
     {
         JFrame myFrame = new JFrame("Joangel");
-        
         myFrame.setSize(200, 200);
-        myFrame.setVisible(true);
-        //myFrame.setLocation(200,200);
-        
+        myFrame.setVisible(true);        
         myFrame.setLocationRelativeTo(null);
         myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
