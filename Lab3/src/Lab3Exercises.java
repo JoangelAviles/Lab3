dd272500e669af12602263e18bb476c37a951405
Chino777 <Chino777@10.24.29.114>
2017-11-15 16:59:45 -0400

    Added New Class
    MyPanelClass was added to draw and paint a background with borders. In
    Lab3Exercises was added the instance object called myPanel.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,12 +2,16 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises 
 {
-    public static void main(String[] args) 
-    {
-        JFrame myFrame = new JFrame("Joangel");
-        myFrame.setSize(200, 200);
-        myFrame.setVisible(true);        
-        myFrame.setLocationRelativeTo(null);
-        myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    }
+	public static void main(String[] args) 
+	{
+		JFrame myFrame = new JFrame("Joangel");
+		myFrame.setSize(200, 200);
+		
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel); 
+        
+		myFrame.setVisible(true);
+		myFrame.setLocationRelativeTo(null);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
 }
