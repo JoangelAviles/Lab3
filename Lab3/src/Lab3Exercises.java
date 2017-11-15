f3b73549420dd64fbe52d20eed9fac2dd272397f
Chino777 <Chino777@10.24.29.114>
2017-11-15 16:46:19 -0400

    FirstEditing
    Changes the name and the position of the frame to the center of the
    display.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,16 @@
 import javax.swing.JFrame;
 
-public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
-	}
+public class Lab3Exercises 
+{
+    public static void main(String[] args) 
+    {
+        JFrame myFrame = new JFrame("Joangel");
+        
+        myFrame.setSize(200, 200);
+        myFrame.setVisible(true);
+        //myFrame.setLocation(200,200);
+        
+        myFrame.setLocationRelativeTo(null);
+        myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
 }
